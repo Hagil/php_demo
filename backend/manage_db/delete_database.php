02e39ff36d1791c19c806bfe3a8e08f9e18faf5b
Hagil Baik <hagilbaik@gmail.com>
2017-10-18 15:06:15 +0800

    In the realm of scientific observation, luck is granted only to those who are prepared. - Louis Pasteur

--- backend/manage_db/delete_database.php
+++ backend/manage_db/delete_database.php
@@ -5,15 +5,23 @@ require_once ('../common/connection.php');
 $db = new Dbconnect($admin, $pass);
 
 $db_name = $_GET['db_name'];
-
-$sql = "drop database $db_name";
-
-$result = $db->conn->query($sql);
-
-if($result){
-  logger("deleted database");
-  echo "dropped database $db_name";
-}
-  else{
-logger($db->conn->error);
+function safety($db_name){
+  $restricted_names = ['project_two', 'mysql', 'performance_schema', 'sys', 'information_schema', 'wdi'];
+  if (in_array($db_name, $restricted_names)){
+    logger('if is true');
+    echo "$db_name cannot be deleted!";
+  } else {
+    logger('if is false');
+    global $db;
+    $sql = "drop database $db_name";
+    $result = $db->conn->query($sql);
+    if($result){
+      logger("deleted database");
+      echo "dropped database $db_name";
   }
+  else{
+    logger($db->conn->error);
+}
+}
+  }
+safety($db_name);
--- backend/manage_users/delete_user.php
+++ backend/manage_users/delete_user.php
@@ -5,15 +5,23 @@ require_once ('../common/connection.php');
 $user = new Dbconnect($admin, $pass);
 
 $user_name = $_GET['user_name'];
-
-$sql = "drop user $user_name";
-
-$result = $user->conn->query($sql);
-
-if($result){
-  logger("deleted user");
-  echo "dropped user $user_name";
-}
-  else{
-logger($user->conn->error);
+function safety($user_name){
+  $restricted_names = ['root', 'mysql.session', 'mysql.sys', 'libby'];
+  if (in_array($user_name, $restricted_names)){
+    logger('if is true');
+    echo "$user_name cannot be deleted!";
+  } else {
+    logger('if is false');
+    global $user;
+    $sql = "drop user $user_name";
+    $result = $user->conn->query($sql);
+    if($result){
+      logger("deleted user");
+      echo "dropped user $user_name";
   }
+  else{
+    logger($user->conn->error);
+}
+}
+  }
+safety($user_name);
